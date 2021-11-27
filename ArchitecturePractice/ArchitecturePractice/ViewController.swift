4fbfe17231c0d503193529b1a3166015d576fb78
Кирилл Проко <prokyhouse@mail.ru>
2021-11-27 15:11:43 +0700

    Создание проекта

---
+++ ArchitecturePractice/ArchitecturePractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ArchitecturePractice
+//
+//  Created by Кирилл Прокофьев on 27.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
