42ba60c407fc9b199912853e7a516bb2502b544e
Кирилл Проко <prokyhouse@mail.ru>
2021-11-27 16:25:15 +0700

    (Screen 3) Добавил VC и модель

--- ArchitecturePractice/ArchitecturePractice/BlogPost/BlogPostModel.swift
+++ ArchitecturePractice/ArchitecturePractice/BlogPost/BlogPostModel.swift
@@ -8,16 +8,43 @@
 import Foundation
 
 final class BlogPostModel {
-//	private var someData: String?
-//
-//	func getData() -> String {
-//		guard let someData = self.someData else {
-//			return ""
-//		}
-//		return someData
-//	}
-//
-//	func setData(data: String) {
-//		self.someData = data
-//	}
+	private var imageName: String?
+	private var title: String?
+	private var author: String?
+	private var text: String?
+
+	func setData(imageName: String, title: String, author: String, text: String) {
+		self.imageName = imageName
+		self.title = title
+		self.author = author
+		self.text = text
+	}
+
+	func getImageName() -> String {
+		guard let imageName = self.imageName else {
+			return "DefaultImage"
+		}
+		return imageName
+	}
+
+	func getTitle() -> String {
+		guard let title = self.title else {
+			return "Нет заголока"
+		}
+		return title
+	}
+
+	func getAuthor() -> String {
+		guard let author = self.author else {
+			return "нет автора"
+		}
+		return author
+	}
+
+	func getText() -> String {
+		guard let text = self.text else {
+			return "Текст отсутствует"
+		}
+		return text
+	}
 }
--- ArchitecturePractice/ArchitecturePractice/BlogPost/BlogPostViewController.swift
+++ ArchitecturePractice/ArchitecturePractice/BlogPost/BlogPostViewController.swift
@@ -8,5 +8,33 @@
 import UIKit
 
 final class BlogPostViewController: UIViewController {
+	private var blogPostView: BlogPostView?
+	private var blogPostModel: BlogPostModel?
 
+	init() {
+		self.blogPostView = BlogPostView(frame: UIScreen.main.bounds)
+		self.blogPostModel = BlogPostModel()
+		super.init(nibName: nil, bundle: nil)
+	}
+
+	required init?(coder: NSCoder) {
+		fatalError("init(coder:) has not been implemented")
+	}
+
+	override func loadView() {
+		super.loadView()
+		self.blogPostView?.loadView(controller: self)
+	}
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+	}
+
+	override func viewDidAppear(_ animated: Bool) {
+		self.blogPostView?.configure()
+		self.blogPostView?.backgroundColor = .white
+		if let blogPostView = blogPostView {
+			self.view.addSubview(blogPostView)
+		}
+	}
 }
