d5afb4e22f3fa32dddff301ab2675a81f36166a9
red-beeard <nifon1998@yandex.ru>
2021-12-04 13:01:34 +0300

    Добавил scroll view

--- ArchitecturePractice/ArchitecturePractice/BlogPost/BlogPostView.swift
+++ ArchitecturePractice/ArchitecturePractice/BlogPost/BlogPostView.swift
@@ -10,93 +10,109 @@ import UIKit
 import SnapKit
 
 final class BlogPostView: UIView {
-	private weak var controller: BlogPostViewController?
-
-	private lazy var imageBlock: UIImageView = {
-		let obj = UIImageView()
-		obj.backgroundColor = .gray
-		obj.layer.cornerRadius = 8
-		obj.clipsToBounds = true
-		obj.contentMode = .scaleAspectFill
-		return obj
-	}()
-
-	private lazy var title: UILabel = {
-		let obj = UILabel()
-		obj.font = UIFont.systemFont(ofSize: 30, weight: .medium)
-		obj.numberOfLines = 1
-		obj.adjustsFontSizeToFitWidth = true
-		obj.textAlignment = .left
-		return obj
-	}()
-
-	private lazy var author: UILabel = {
-		let obj = UILabel()
-		obj.font = UIFont.systemFont(ofSize: 16, weight: .semibold)
-		obj.numberOfLines = 1
-		obj.adjustsFontSizeToFitWidth = true
-		obj.textAlignment = .left
-		return obj
-	}()
-
-	private lazy var text: UILabel = {
-		let obj = UILabel()
-		obj.font = UIFont.systemFont(ofSize: 16, weight: .medium)
-		obj.numberOfLines = 20
-		obj.sizeToFit()
-		obj.adjustsFontSizeToFitWidth = true
-		obj.textAlignment = .left
-		return obj
-	}()
-
-	func configure() {
-		self.addSubviews()
-		self.setConstraints()
-	}
-
-	private func addSubviews() {
-		self.addSubview(self.imageBlock)
-		self.addSubview(self.title)
-		self.addSubview(self.author)
-		self.addSubview(self.text)
-	}
-
-	private func setConstraints() {
-		self.imageBlock.snp.makeConstraints { make in
-			make.left.right.equalToSuperview().inset(16)
-			make.top.equalTo(self.snp.top).offset(90)
-
-			make.height.equalTo(196)
-		}
-
-		self.title.snp.makeConstraints { make in
-			make.left.right.equalToSuperview().inset(16)
-			make.top.equalTo(self.imageBlock.snp.bottom).offset(32)
-
-			make.height.lessThanOrEqualTo(29)
-		}
-
-		self.author.snp.makeConstraints { make in
-			make.left.right.equalToSuperview().inset(16)
-			make.top.equalTo(self.title.snp.bottom).offset(8)
-
-			make.height.lessThanOrEqualTo(19)
-		}
-
-		self.text.snp.makeConstraints { make in
-			make.left.right.equalToSuperview().inset(16)
-			make.top.equalTo(self.author.snp.bottom).offset(16)
-		}
-	}
-
-	func loadView(controller: BlogPostViewController) {
-		self.controller = controller
-	}
-
-	func update(imageName: String, title: String?, author: String?, text: String?) {
-		self.imageBlock.image = UIImage(named: imageName)
-		self.title.text = title
-		self.author.text = author
-		self.text.text = text
-	}
+    private weak var controller: BlogPostViewController?
+    
+    private lazy var imageBlock: UIImageView = {
+        let obj = UIImageView()
+        obj.backgroundColor = .gray
+        obj.layer.cornerRadius = 8
+        obj.clipsToBounds = true
+        obj.contentMode = .scaleAspectFill
+        return obj
+    }()
+    
+    private lazy var title: UILabel = {
+        let obj = UILabel()
+        obj.font = UIFont.systemFont(ofSize: 30, weight: .medium)
+        obj.numberOfLines = 1
+        obj.adjustsFontSizeToFitWidth = true
+        obj.textAlignment = .left
+        return obj
+    }()
+    
+    private lazy var author: UILabel = {
+        let obj = UILabel()
+        obj.font = UIFont.systemFont(ofSize: 16, weight: .semibold)
+        obj.numberOfLines = 1
+        obj.adjustsFontSizeToFitWidth = true
+        obj.textAlignment = .left
+        return obj
+    }()
+    
+    private lazy var scrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.showsVerticalScrollIndicator = false
+        return scrollView
+    }()
+    
+    private lazy var text: UILabel = {
+        let obj = UILabel()
+        obj.font = UIFont.systemFont(ofSize: 16, weight: .medium)
+        obj.numberOfLines = 0
+        obj.sizeToFit()
+        obj.adjustsFontSizeToFitWidth = true
+        obj.textAlignment = .left
+        return obj
+    }()
+    
+    func configure() {
+        self.addSubviews()
+        self.setConstraints()
+    }
+    
+    private func addSubviews() {
+        self.addSubview(self.imageBlock)
+        self.addSubview(self.scrollView)
+    
+        self.scrollView.addSubview(self.title)
+        self.scrollView.addSubview(self.author)
+        self.scrollView.addSubview(self.text)
+    }
+    
+    private func setConstraints() {
+        self.imageBlock.snp.makeConstraints { make in
+            make.left.right.equalToSuperview().inset(16)
+            make.top.equalTo(self.snp.top).offset(90)
+            
+            make.height.equalTo(196)
+        }
+        
+        self.scrollView.snp.makeConstraints { make in
+            make.left.right.equalToSuperview().inset(16)
+            make.top.equalTo(self.imageBlock.snp.bottom).offset(32)
+            make.bottom.equalToSuperview().offset(-32)
+        }
+        
+        self.title.snp.makeConstraints { make in
+            make.left.right.equalToSuperview()
+            make.top.equalToSuperview()
+            
+            make.height.lessThanOrEqualTo(29)
+        }
+        
+        self.author.snp.makeConstraints { make in
+            make.left.right.equalToSuperview()
+            make.top.equalTo(self.title.snp.bottom).offset(8)
+            
+            make.height.lessThanOrEqualTo(19)
+        }
+        
+        self.text.snp.makeConstraints { make in
+            make.top.equalTo(self.author.snp.bottom).offset(16)
+            make.width.equalToSuperview()
+            
+            make.bottom.equalToSuperview()
+        }
+    }
+    
+    func loadView(controller: BlogPostViewController) {
+        self.controller = controller
+    }
+    
+    func update(imageName: String, title: String?, author: String?, text: String?) {
+        self.imageBlock.image = UIImage(named: imageName)
+        self.title.text = title
+        self.author.text = author
+        self.text.text = text
+    }
 }
